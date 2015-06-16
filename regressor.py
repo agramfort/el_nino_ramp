6f8cb1acd368278f19c2fba3087c6ab5c4836ffd
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2015-06-16 14:22:44 +0200

    wip feature agglo

--- regressor.py
+++ regressor.py
@@ -4,33 +4,30 @@ from sklearn.feature_selection import SelectKBest, f_regression
 # from sklearn.pipeline import make_pipeline
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import KernelPCA, PCA
+from sklearn.decomposition import PCA
 
 
 def apply_mask(X, mask):
-    n_samples, n_lags, n_lats, n_lons = X.shape
-    X = X.reshape(n_samples, n_lags, n_lats * n_lons)
     X = X[:, :, mask]
-    X = X.reshape(n_samples, -1)
+    X = X.reshape(len(X), -1)
     return X
 
 N_JOBS = -1
 
+
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = Pipeline(
-            [
-             ('scaler', StandardScaler()),
-             ('pca', PCA(n_components=20)),
-             ("RF", RandomForestRegressor(n_estimators=100, max_depth=15,
-                                          n_jobs=N_JOBS))
-             ])
+        self.clf = Pipeline([
+            # ('scaler', StandardScaler()),
+            # ('pca', PCA(n_components=20)),
+            ("RF", RandomForestRegressor(n_estimators=100, max_depth=15,
+                                         n_jobs=N_JOBS))])
 
     def fit(self, X, y):
         y = y.ravel()
-        n_samples, n_lags, n_lats, n_lons = X.shape
-        X1 = X[:, -1].reshape(n_samples, n_lats * n_lons)
-        select = SelectKBest(f_regression, k=1500)
+        n_samples, n_lags, n_clusters = X.shape
+        X1 = X[:, -1].reshape(n_samples, n_clusters)
+        select = SelectKBest(f_regression, k=min(1500, n_clusters))
         select.fit(X1, y)
         mask = select._get_support_mask()
         X = apply_mask(X, mask)
--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -1,4 +1,7 @@
 import numpy as np
+from sklearn.feature_extraction.image import grid_to_graph
+from sklearn import cluster
+from sklearn.preprocessing import StandardScaler
 
 random_state = 61
 n_burn_in = 120
@@ -25,8 +28,19 @@ class FeatureExtractor(object):
         El Nino 3.4 region."""
         data = temperatures_xray['tas'].values
         n_times, n_lats, n_lons = data.shape
-        n_locations = n_lats * n_lons
+        # n_locations = n_lats * n_lons
         valid_range = np.arange(n_burn_in, n_times - n_lookahead)
+
+        data = data.reshape(n_times, -1)
+        connectivity = grid_to_graph(n_lats, n_lons)
+        agglo = cluster.FeatureAgglomeration(connectivity=connectivity,
+                                             n_clusters=500)
+                                             # n_clusters=2664)
+        scaler = StandardScaler()
+        data = scaler.fit_transform(data)
+        data = agglo.fit_transform(data)
+        self.agglo_ = agglo
+
         X = []
         for k in valid_range:
             X.append(data[k - 2:k + 1])
@@ -36,15 +50,27 @@ class FeatureExtractor(object):
 if __name__ == '__main__':
     import xray
     resampled_xray = xray.open_dataset('resampled_xray.nc')
-# resampled_xray = xray.open_dataset('COLA_data/resampled_tas_Amon_CCSM4_piControl_r1i1p1_080001-130012.nc')
 
     random_state = 61
     n_burn_in = 120
     n_lookahead = 6
 
-    valid_range = range(n_burn_in, resampled_xray['time'].shape[0] - n_lookahead)
-    y_array = resampled_xray['target'][valid_range].values
+    idx = np.arange(n_burn_in, resampled_xray['time'].shape[0] - n_lookahead)
+    y_array = resampled_xray['target'][idx].values
     y = y_array.reshape((y_array.shape[0], 1))
     fe = FeatureExtractor()
     X = fe.transform(resampled_xray, n_burn_in, n_lookahead, None)
 
+    # Look at univariate scores
+    from sklearn.feature_selection import f_regression
+    F, pv = f_regression(X[:, -1, :], y)
+    F = fe.agglo_.inverse_transform(F)
+
+    from viz import plot_map
+
+    ds = resampled_xray
+    _, n_lats, n_lons = ds['tas'].values.shape
+    F_ds = xray.Dataset(
+        {'tas': (['time', 'lat', 'lon'], F.reshape(1, n_lats, n_lons))},
+        coords={'time': ds['time'][:1], 'lat': ds['lat'], 'lon': ds['lon']})
+    plot_map(F_ds['tas'], 0)
