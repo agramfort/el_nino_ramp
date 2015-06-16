f0e1de18ddd2c2593c9c32374006878f2121f417
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2015-06-16 14:51:25 +0200

    working agglo

--- regressor.py
+++ regressor.py
@@ -1,16 +1,10 @@
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.base import BaseEstimator
-from sklearn.feature_selection import SelectKBest, f_regression
-# from sklearn.pipeline import make_pipeline
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import PCA
+from sklearn.cluster import FeatureAgglomeration
+from sklearn.feature_extraction.image import grid_to_graph
 
-
-def apply_mask(X, mask):
-    X = X[:, :, mask]
-    X = X.reshape(len(X), -1)
-    return X
 
 N_JOBS = -1
 
@@ -18,22 +12,27 @@ N_JOBS = -1
 class Regressor(BaseEstimator):
     def __init__(self):
         self.clf = Pipeline([
-            # ('scaler', StandardScaler()),
-            # ('pca', PCA(n_components=20)),
-            ("RF", RandomForestRegressor(n_estimators=100, max_depth=15,
+            ("RF", RandomForestRegressor(n_estimators=200, max_depth=15,
                                          n_jobs=N_JOBS))])
+        self.scaler = StandardScaler()
+        self.agglo = FeatureAgglomeration(n_clusters=500)
 
     def fit(self, X, y):
         y = y.ravel()
-        n_samples, n_lags, n_clusters = X.shape
-        X1 = X[:, -1].reshape(n_samples, n_clusters)
-        select = SelectKBest(f_regression, k=min(1500, n_clusters))
-        select.fit(X1, y)
-        mask = select._get_support_mask()
-        X = apply_mask(X, mask)
-        self.mask_ = mask
+        n_samples, n_lags, n_lats, n_lons = X.shape
+        self.scaler.fit(X[:, -1].reshape(n_samples, -1))
+        X = X.reshape(n_lags * n_samples, -1)
+        connectivity = grid_to_graph(n_lats, n_lons)
+        self.agglo.connectivity = connectivity
+        X = self.scaler.transform(X)
+        X = self.agglo.fit_transform(X)
+        X = X.reshape(n_samples, -1)
         self.clf.fit(X, y)
 
     def predict(self, X):
-        X = apply_mask(X, self.mask_)
+        n_samples, n_lags, n_lats, n_lons = X.shape
+        X = X.reshape(n_lags * n_samples, -1)
+        X = self.scaler.transform(X)
+        X = self.agglo.transform(X)
+        X = X.reshape(n_samples, -1)
         return self.clf.predict(X)
