3be2b698b7c2ebde652c917247fe1bdd1427b5ac
Alexandre Gramfort <alexandre.gramfort@m4x.org>
2015-06-16 13:50:22 +0200

    start to add history

--- regressor.py
+++ regressor.py
@@ -4,19 +4,39 @@ from sklearn.feature_selection import SelectKBest, f_regression
 # from sklearn.pipeline import make_pipeline
 from sklearn.pipeline import Pipeline
 from sklearn.preprocessing import StandardScaler
-from sklearn.decomposition import KernelPCA
+from sklearn.decomposition import KernelPCA, PCA
 
+
+def apply_mask(X, mask):
+    n_samples, n_lags, n_lats, n_lons = X.shape
+    X = X.reshape(n_samples, n_lags, n_lats * n_lons)
+    X = X[:, :, mask]
+    X = X.reshape(n_samples, -1)
+    return X
+
+N_JOBS = -1
 
 class Regressor(BaseEstimator):
     def __init__(self):
-        self.clf = Pipeline([('scaler', StandardScaler()),
-                             ('anova', SelectKBest(f_regression, k=1500)),
-                             ('KernelPCA', KernelPCA(n_components=10)),
-                             ("RF", RandomForestRegressor(n_estimators=200, max_depth=15))])
+        self.clf = Pipeline(
+            [
+             ('scaler', StandardScaler()),
+             ('pca', PCA(n_components=20)),
+             ("RF", RandomForestRegressor(n_estimators=100, max_depth=15,
+                                          n_jobs=N_JOBS))
+             ])
 
     def fit(self, X, y):
         y = y.ravel()
+        n_samples, n_lags, n_lats, n_lons = X.shape
+        X1 = X[:, -1].reshape(n_samples, n_lats * n_lons)
+        select = SelectKBest(f_regression, k=1500)
+        select.fit(X1, y)
+        mask = select._get_support_mask()
+        X = apply_mask(X, mask)
+        self.mask_ = mask
         self.clf.fit(X, y)
 
     def predict(self, X):
+        X = apply_mask(X, self.mask_)
         return self.clf.predict(X)
--- ts_feature_extractor.py
+++ ts_feature_extractor.py
@@ -18,13 +18,33 @@ class FeatureExtractor(object):
     def fit(self, temperatures_xray, n_burn_in, n_lookahead):
         pass
 
-    def transform(self, temperatures_xray, n_burn_in=n_burn_in, n_lookahead=n_lookahead,
-                  skf_is=None):
-        """Combine two variables: the montly means corresponding to the month of the target and 
-        the current mean temperature in the El Nino 3.4 region."""
+    def transform(self, temperatures_xray, n_burn_in=n_burn_in,
+                  n_lookahead=n_lookahead, skf_is=None):
+        """Combine two variables: the montly means corresponding to the
+        month of the target and the current mean temperature in the
+        El Nino 3.4 region."""
         data = temperatures_xray['tas'].values
         n_times, n_lats, n_lons = data.shape
         n_locations = n_lats * n_lons
         valid_range = np.arange(n_burn_in, n_times - n_lookahead)
-        X = data[valid_range].reshape((-1, n_lats * n_lons))
+        X = []
+        for k in valid_range:
+            X.append(data[k - 2:k + 1])
+        X = np.array(X)
         return X
+
+if __name__ == '__main__':
+    import xray
+    resampled_xray = xray.open_dataset('resampled_xray.nc')
+# resampled_xray = xray.open_dataset('COLA_data/resampled_tas_Amon_CCSM4_piControl_r1i1p1_080001-130012.nc')
+
+    random_state = 61
+    n_burn_in = 120
+    n_lookahead = 6
+
+    valid_range = range(n_burn_in, resampled_xray['time'].shape[0] - n_lookahead)
+    y_array = resampled_xray['target'][valid_range].values
+    y = y_array.reshape((y_array.shape[0], 1))
+    fe = FeatureExtractor()
+    X = fe.transform(resampled_xray, n_burn_in, n_lookahead, None)
+
